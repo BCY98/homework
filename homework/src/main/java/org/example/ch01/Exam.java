7538ecbe1e0dc930935bcf62b95bd24a828e53ac
BCY <gkfnal0120@naver.com>
2024-03-18 11:23:33 +0900

    완성

--- homework/src/main/java/org/example/ch01/Exam.java
+++ homework/src/main/java/org/example/ch01/Exam.java
@@ -3,7 +3,6 @@ package org.example.ch01;
 public class Exam {
     public static void main(String[] args) {
         System.out.println("과제 코드 작성");
-        System.out.println("과제 진행 중");
         System.out.println("과제 완료");
     }
 }
