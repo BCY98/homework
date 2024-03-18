38cd5a324b4a3b6dccd6fc8785bd7c9fd1c91a8e
BCY <gkfnal0120@naver.com>
2024-03-18 11:32:11 +0900

    11

--- homework/src/main/java/org/example/ch01/Exam.java
+++ homework/src/main/java/org/example/ch01/Exam.java
@@ -3,7 +3,7 @@ package org.example.ch01;
 public class Exam {
     public static void main(String[] args) {
         System.out.println("과제 코드 작성");
-        System.out.println("실습");
+        System.out.println("실습1");
         System.out.println("과제 완료");
     }
 }
